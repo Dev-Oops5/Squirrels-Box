96672cef80e898e9830f42847f8e0e1c32b3dbea
DiegoHZ5 <safairus@gmail.com>
2022-09-05 03:38:50 -0500

    Avance de navigation view
    Se hace diseño de profile y settings

---
+++ app/src/main/java/com/miodemi/squirrelsbox/ui/settings/MenuSettingsFragment.kt
@@ -0,0 +1,31 @@
+package com.miodemi.squirrelsbox.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.miodemi.squirrelsbox.R
+import com.miodemi.squirrelsbox.databinding.FragmentMenuSettingsBinding
+
+class MenuSettingsFragment : Fragment() {
+
+    //binding
+    internal lateinit var binding: FragmentMenuSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        //init data binding in a fragment
+        binding = FragmentMenuSettingsBinding.inflate(layoutInflater)
+        //this value must be returned
+        val view : View = binding.root
+
+        //--Insert actions here--//
+
+        // Inflate the layout for this fragment
+        return view
+    }
+
+}
