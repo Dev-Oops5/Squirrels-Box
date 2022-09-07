76f1ad0fb50f16c1315640f5a12a2525654c8f01
DiegoHZ5 <safairus@gmail.com>
2022-09-06 20:43:06 -0500

    avance en front
    se realizan avances en la parte de verificacion de nro clular

--- app/src/main/java/com/miodemi/squirrelsbox/fragments/RegisterMobileFragment.kt
+++ app/src/main/java/com/miodemi/squirrelsbox/fragments/RegisterMobileFragment.kt
@@ -1,13 +1,17 @@
 package com.miodemi.squirrelsbox.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.miodemi.squirrelsbox.R
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import com.miodemi.squirrelsbox.databinding.FragmentRegisterMobileBinding
-import com.miodemi.squirrelsbox.databinding.FragmentRegisterUserInfoBinding
+import kotlinx.android.synthetic.main.fragment_register_mobile.*
+
 
 class RegisterMobileFragment : Fragment() {
 
@@ -23,7 +27,40 @@ class RegisterMobileFragment : Fragment() {
         //this value must be returned
         val view : View = binding.root
 
-        //--Insert actions here--//
+        //Edit text hop
+        binding.code1Et.addTextChangedListener {
+            if (binding.code1Et.text.toString().length == 1)
+                binding.code2Et.requestFocus()
+        }
+        binding.code2Et.addTextChangedListener {
+            if (binding.code2Et.text.toString().length == 1)
+                binding.code3Et.requestFocus()
+        }
+        binding.code3Et.addTextChangedListener {
+            if (binding.code3Et.text.toString().length == 1)
+                binding.code4Et.requestFocus()
+        }
+        binding.code4Et.addTextChangedListener {
+            if (binding.code4Et.text.toString().length == 1)
+                binding.code5Et.requestFocus()
+        }
+        binding.code5Et.addTextChangedListener {
+            if (binding.code5Et.text.toString().length == 1)
+                binding.code6Et.requestFocus()
+        }
+        //Sending action to confirm code
+        binding.code6Et.addTextChangedListener {
+            if (binding.code6Et.text.toString().length == 1){
+//                binding.code1Et.text.clear()
+//                binding.code2Et.text.clear()
+//                binding.code3Et.text.clear()
+//                binding.code4Et.text.clear()
+//                binding.code5Et.text.clear()
+//                binding.code6Et.text.clear()
+                Toast.makeText(this.activity , "Automatically sent", Toast.LENGTH_SHORT).show()
+            }
+
+        }
 
         // Inflate the layout for this fragment
         return view
