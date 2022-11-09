1120a09c5380c89bb6d133259fefcd44e8a96813
RasecOri <83627310+RasecOri@users.noreply.github.com>
2022-11-09 04:43:27 -0500

    added-update-profileimg

--- app/src/main/java/com/miodemi/squirrelsbox/profile/presentation/profile/MenuProfileFragment.kt
+++ app/src/main/java/com/miodemi/squirrelsbox/profile/presentation/profile/MenuProfileFragment.kt
@@ -1,10 +1,15 @@
 package com.miodemi.squirrelsbox.profile.presentation.profile
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import com.miodemi.squirrelsbox.databinding.FragmentMenuProfileBinding
 
 class MenuProfileFragment : Fragment() {
@@ -13,6 +18,8 @@ class MenuProfileFragment : Fragment() {
     internal lateinit var binding: FragmentMenuProfileBinding
     private var actBox = 5
     private var maxBox = 10
+
+    lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,6 +40,42 @@ class MenuProfileFragment : Fragment() {
 
         // Inflate the layout for this fragment
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        if(user != null){
+            binding.usernameEt.setText(user.email)
+        }
+
+        binding.profilePicIv.setOnClickListener {
+            openGalery()
+        }
+    }
+
+    private val startForActivityGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+        result->
+        if(result.resultCode == Activity.RESULT_OK){
+            val data = result.data?.data
+            binding.profilePicIv.setImageURI(data)
+        }
+    }
+
+
+    private fun openGalery(){
+
+        val intent = Intent()
+
+        intent.action = Intent.ACTION_GET_CONTENT
+
+        intent.type = "image/*"
+
+        startForActivityGallery.launch(intent)
     }
 
 }
